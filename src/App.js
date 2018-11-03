f9a1b7a5f2159408043e6ce0f8c982d9e3306351
LikeMyBread <mbradley31@outlook.com>
2018-11-02 23:11:20 -0700

    Roughed out sprite candidates.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // @flow
 import React, { Component } from 'react';
 import './App.css';
+import { randomSprite } from './spriteHelper';
+import { capitalizeFirst } from './utils';
 
 class App extends Component {
   render() {
+    const candidate = randomSprite();
     return (
       <main>
         <aside>
@@ -14,6 +17,16 @@ class App extends Component {
         </section>
         <aside>
           Find new friends
+          <img
+            className='candidate'
+            src={`/img/sprites/${candidate.species}/${candidate.variant}.png`}
+            alt={`${candidate.variant} ${candidate.species}`}
+          />
+          <figcaption>
+            {`${candidate.name} the ${candidate.variant} ${candidate.species}`}
+          </figcaption>
+          <div>{`Recruit ${candidate.name} for 500 Beans.`}</div>
+          <div>{`Dismiss ${candidate.name} for 50 Beans.`}</div>
         </aside>
       </main>
     );
