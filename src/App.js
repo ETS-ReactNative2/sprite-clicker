ae9df6004ba76e57710c175edd2cc868764e3295
Tiffany Zhang <zhangtiff@google.com>
2018-11-02 23:05:23 -0700

    Add clicking to get beans.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,44 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beans: 100,
+    };
+
+    this.clickForBeans = this.clickForBeans.bind(this);
+  }
+
+  clickForBeans() {
+    this.setState({beans: this.state.beans + 1});
+  }
+
   render() {
     return (
       <main>
         <aside>
-          1000 beans
+          <h2>
+            { this.state.beans } beans
+          </h2>
+          <h3>
+            0 Beans per second
+          </h3>
         </aside>
         <section>
-          Arky the Arko
+          <h2>
+            Arky the Arko
+          </h2>
+          <h3>
+            Arky patiently waits for you to pet him.
+          </h3>
+          <img
+            className="active-sprite"
+            onClick={this.clickForBeans}
+            src="/img/sprites/arko/saylian.png"
+            alt="Arky the Arko"
+            title="Arky the Arko"
+          />
         </section>
         <aside>
           Find new friends
